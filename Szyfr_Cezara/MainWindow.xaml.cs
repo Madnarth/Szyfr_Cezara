e8d6090e4a529c9918d5a0dba2bf70596031ac11
Madnarth <corwyn.blade@gmail.com>
2017-11-09 00:05:23 +0100

    Działająca wersja

--- Szyfr_Cezara/MainWindow.xaml.cs
+++ Szyfr_Cezara/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,88 @@ namespace Szyfr_Cezara
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        char[] alfabet = new char[32] { 'a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'r', 's', 'ś', 't', 'u', 'w', 'y', 'z', 'ź', 'ż', };
+
+        private void szyfruj(object sender, RoutedEventArgs e)
+        {
+            int przesuniecie = 0;
+            char znak = '0';
+            if (Int32.Parse(TxtBoxPrzesuniecie.Text) > 31)
+                TxtBoxPrzesuniecie.Text = "31";
+
+            if (Int32.Parse(TxtBoxPrzesuniecie.Text) != 0)
+            {
+                    TxtBoxSzyf.Text = "";
+                    foreach (char c in TxtBoxNieszyf.Text)
+                    {
+                        znak = c;
+
+                        if (Char.IsLetter(znak))
+                        {
+                            przesuniecie = Array.IndexOf(alfabet, znak) + int.Parse(TxtBoxPrzesuniecie.Text);
+
+                            if (przesuniecie >= 32)
+                                przesuniecie = przesuniecie - 32;
+
+                            TxtBoxSzyf.Text += alfabet[przesuniecie];
+                        }
+                        else
+                        {
+                            TxtBoxSzyf.Text += znak;
+                        }
+                    }                
+            }
+            else
+            {
+                TxtBoxSzyf.Text = TxtBoxNieszyf.Text;
+            }
+        }
+
+        private void TxtBoxPrzesuniecie_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(TxtBoxPrzesuniecie.Text, "[^0-9]"))
+            {
+                TxtBoxPrzesuniecie.Text = TxtBoxPrzesuniecie.Text.Remove(TxtBoxPrzesuniecie.Text.Length - 1);
+            }
+        }
+
+        private void deszyfruj(object sender, RoutedEventArgs e)
+        {
+            int przesuniecie = 0;
+            char znak = '0';
+            if (Int32.Parse(TxtBoxPrzesuniecie.Text) > 31)
+                TxtBoxPrzesuniecie.Text = "31";
+
+            if (Int32.Parse(TxtBoxPrzesuniecie.Text) != 0)
+            {
+                    TxtBoxNieszyf_Copy.Text = "";
+                    foreach (char c in TxtBoxSzyf_Copy.Text)
+                    {
+                        znak = c;
+
+                        if (Char.IsLetter(znak))
+                        {
+                            przesuniecie = Array.IndexOf(alfabet, znak) - int.Parse(TxtBoxPrzesuniecie.Text);
+
+                            if (przesuniecie < 0)
+                                przesuniecie = przesuniecie + 32;
+                            if (przesuniecie == 0)
+                            przesuniecie = 0;
+
+                        TxtBoxNieszyf_Copy.Text += alfabet[przesuniecie];
+                        }
+                        else
+                        {
+                            TxtBoxNieszyf_Copy.Text += znak;
+                        }
+                    }         
+            }
+            else
+            {
+                TxtBoxNieszyf_Copy.Text = TxtBoxSzyf_Copy.Text;
+            }
         }
     }
 }
